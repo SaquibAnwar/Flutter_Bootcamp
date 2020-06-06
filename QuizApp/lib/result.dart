39f99b81dd1b59ce9c0e18d87aff5fe95c8d72a5
SaquibAnwar <saquibedu@gmail.com>
2020-06-06 11:28:21 +0530

    Splitting the app into widgets

---
+++ QuizApp/lib/result.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class Result extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('You did it!'),
+    );
+  }
+}
