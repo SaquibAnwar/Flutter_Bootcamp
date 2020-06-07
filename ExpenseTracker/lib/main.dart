0e53cc33a6dd8ed44a3ab46c67d048bf6a422584
SaquibAnwar <saquibedu@gmail.com>
2020-06-07 12:58:43 +0530

    adding input functionality

--- ExpenseTracker/lib/main.dart
+++ ExpenseTracker/lib/main.dart
@@ -30,6 +30,10 @@ class MyHomePage extends StatelessWidget {
       date: DateTime.now(),
     ),
   ];
+  // String titleInput;
+  // String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -53,13 +57,26 @@ class MyHomePage extends StatelessWidget {
             child: Container(
               padding: EdgeInsets.all(10),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
                 children: <Widget>[
                   TextField(
                     decoration: InputDecoration(labelText: 'Title'),
+                    controller: titleController,
+                    // onChanged: (val) => titleInput = val,
                   ),
                   TextField(
                     decoration: InputDecoration(labelText: 'Amount'),
+                    controller: amountController,
+                    // onChanged: (val) => amountInput = val,
                   ),
+                  FlatButton(
+                    onPressed: () {
+                      print(titleController.text);
+                      print(amountController.text);
+                    },
+                    textColor: Colors.purple,
+                    child: Text('Add transaction'),
+                  )
                 ],
               ),
             ),
