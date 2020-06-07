1555b83c1a674aed134843eb58cc755ba4a173cb
SaquibAnwar <saquibedu@gmail.com>
2020-06-07 14:52:39 +0530

    Putting add button on ui

--- ExpenseTracker/lib/main.dart
+++ ExpenseTracker/lib/main.dart
@@ -25,6 +25,9 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.add), onPressed: () {})
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -43,6 +46,8 @@ class MyHomePage extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(child: Icon(Icons.add), onPressed: () {}),
     );
   }
 }
